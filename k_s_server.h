e4729b644debd29a7b5d8816f973178fa17683c2
aduranova <duranova@isw.sk>
2022-12-22 19:41:28 +0100

    added classes necessary for synchronous communication

---
+++ k_s_server.h
@@ -0,0 +1,8 @@
+//
+// Created by aduranova on 22. 12. 2022.
+//
+
+#ifndef HADIK_SEMESTRALNA_PRACA_K_S_SERVER_H
+#define HADIK_SEMESTRALNA_PRACA_K_S_SERVER_H
+int main(int argc, char** argv);
+#endif //HADIK_SEMESTRALNA_PRACA_K_S_SERVER_H
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+//zvazujem ci nebude treba nejaku upravu podla mnozstva maximalneho ale nie som si ista
 int main(int argc, char** argv) {
     printf("Hello, World!\n");
     return 0;
